fe8227219bfd3929c7b98d7ec9bbfc925d8cc463
ars9ne <arsenyrt@yandex.ru>
2023-11-19 12:48:02 +0300

    Fixed agent detection

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ ax.set_ylim([0, 100])
 
 
 class Point(object):
+    _id_counter = 0
+
     def __init__(self, x, y):
+        self.id = Point._id_counter
+        Point._id_counter += 1
         self.x = x
         self.y = y
-
         self.scat = ax.scatter(x, y, s=3*50, label=f"Point at ({x}, {y})")
 
+        self.text = ax.text(x, y, f"{self.id}", fontsize=9, ha='right')
 
 class Agent(object):
     _id_counter = 0
@@ -30,6 +34,8 @@ class Agent(object):
         self.directionx = np.cos(angle)
         self.directiony = np.sin(angle)
         self.steps_since_collisions = [0, 0]
+        self.text = ax.text(x, y, f"{self.id}", fontsize=9, ha='right')
+
 
     def update_position(self, points):
         self.steps_since_collisions = [x + 1 for x in self.steps_since_collisions]
@@ -42,13 +48,14 @@ class Agent(object):
             self.directiony *= -1
             self.y = max(min(self.y, 100), 0)
         self.scat.set_offsets((self.x, self.y))
+        self.text.set_position((self.x, self.y))
 
     def detect_collision(self, points):
         for point in points:
             distance = np.sqrt((self.x - point.x) ** 2 + (self.y - point.y) ** 2)
             if distance <= 3:
-                self.record_collision(0)
-                self.target_point = point
+                self.record_collision(point.id)
+                self.target_point = points[1] if point.id == 0 else points[0]
                 return True
         return False
 
@@ -57,11 +64,16 @@ class Agent(object):
             if agent.id != self.id:
                 distance = np.sqrt((self.x - agent.x) ** 2 + (self.y - agent.y) ** 2)
                 if distance <= 10:
-                    self.record_collision(1)
-                    if agent.steps_since_collisions[1] < self.steps_since_collisions[1]:
-                        self.steps_since_collisions[1] = agent.steps_since_collisions[1]
-                        if agent.target_point == self.target_point:
+                    if agent.steps_since_collisions[1] + 10 < self.steps_since_collisions[1]:
+                        self.steps_since_collisions[1] = agent.steps_since_collisions[1] + 10
+                        if (agent.target_point == self.target_point) and (agent.steps_since_collisions[1] < self.steps_since_collisions[1] + 10):
                             self.turn_towards(agent.x, agent.y)
+                    elif agent.steps_since_collisions[0] + 10 < self.steps_since_collisions[0]:
+                        self.steps_since_collisions[0] = agent.steps_since_collisions[0] + 10
+                        if (agent.target_point == self.target_point) and (
+                                agent.steps_since_collisions[1] < self.steps_since_collisions[1] + 10):
+                            self.turn_towards(agent.x, agent.y)
+
 
     def turn_towards(self, x, y):
         self.directionx = x - self.x
@@ -74,8 +86,9 @@ class Agent(object):
         self.steps_since_collisions[index] = 0
 
     def announce(self):
-        print(f"Agent {self.id} шагов после столкновения: {self.steps_since_collisions}")
-
+        target_point_id = self.target_point.id if self.target_point else None
+        # pass print(f"Agent {self.id} шагов после столкновения: {self.steps_since_collisions} TARGET POINT ID: {target_point_id}")
+        pass
 
 points = [Point(random.uniform(0, 100), random.uniform(0, 100)) for _ in range(2)]
 agents = [Agent(random.uniform(0, 100), random.uniform(0, 100), target_point=points[0]) for _ in range(50)]
@@ -87,13 +100,14 @@ def update_agents():
         agent.detect_nearby_agents(agents)
         agent.announce()
     fig.canvas.draw_idle()
-    plt.pause(0.1)
+    plt.pause(0.01)
+    #print('\n')
 
 
 
 
 
-max_iterations = 5000
+max_iterations = 500000
 for _ in range(max_iterations):
     update_agents()
 
